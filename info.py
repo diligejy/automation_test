546d5d2cb49d3b2035371ed3c3964f5005985e40
diligejy <sjy049@gmail.com>
2020-09-16 12:02:28 +0900

    first commit

---
+++ info.py
@@ -0,0 +1,45 @@
+import requests
+import time
+import bs4 
+from datetime import datetime
+from datetime import timedelta
+import os
+
+
+today = datetime.now()
+diff = timedelta(days=7)
+base_date = today - diff
+
+def slack_post_text(url, text):
+    result = requests.post(url, json = {"text": text})
+    # {"username": "new-bot-name"}
+    return result
+
+url = os.environ.get('SLACK_URL')
+
+req = requests.get('https://www.nipa.kr/main/selectBbsNttList.do?bbsNo=2&key=122')
+soup = bs4.BeautifulSoup(req.text, 'html.parser')
+
+
+
+matches = []
+elems = soup.select('td.p-subject a')
+# elems = driver.find_elements_by_xpath("//td[@class='p-subject']/a")
+
+dates = soup.select('#contents div table tbody tr td time')
+
+# dates = driver.find_elements_by_xpath('//*[@id="contents"]/div/table/tbody/tr/td/time')
+
+
+for date, elem in zip(dates, elems):
+    d = datetime.strptime(date["datetime"], "%Y-%m-%d")
+    if d > base_date:
+        matches.append(f'{date.text.strip()} : {elem.text.strip()} \n 주소 : {elem["href"].strip()}')
+
+
+if matches:
+    contents = '최근 올라온 공고가 있습니다. \n\n'
+
+    contents += '\n'.join(matches)
+    slack_post_text(url, contents)
+
