791096525b470cea5b98d374aca8ff1a4ca62550
Jinyoung <40735154+diligejy@users.noreply.github.com>
2020-09-17 13:26:20 +0900

    Update info.py

--- info.py
+++ info.py
@@ -37,7 +37,7 @@ dates = soup.select('#contents div table tbody tr td time')
 for date, elem in zip(dates, elems):
     d = datetime.strptime(date["datetime"], "%Y-%m-%d")
     if d > base_date:
-        matches.append(f'{date.text.strip()} : {clean_text(elem.text.strip())} \n')
+        matches.append(f'{date.text.strip()} : {clean_text(elem.text.strip())}')
 
 
 if matches:
